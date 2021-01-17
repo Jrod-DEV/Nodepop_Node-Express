dd96e2d879a42f302931979dd0981affc527d948
Jonathan Rod <jonyrm91@gmail.com>
2021-01-17 02:09:09 +0100

    Filters aplicated on views

--- Nodepop/routes/index.js
+++ Nodepop/routes/index.js
@@ -3,19 +3,63 @@ const router = express.Router();
 
 const Advert = require('../models/Advert');
 
-/* GET home page. */
-router.get('/', async function (req, res, next) {
+/* GET home page(all adverts). */
+router.get('/', async function (req, res) {
   try {
     const arrayAdvertsDB = await Advert.find();
     console.log(arrayAdvertsDB);
 
+    const name = req.query.name;
+    const onsale = req.query.onsale;
+    const price = req.query.price;
+    const tag = req.query.tag;
+
+    const limit = parseInt(req.query.limit || 10);
+    const skip = parseInt(req.query.skip);
+    const sort = req.query.sort || '_id';
+
+    // Search filters
+    const filter = {};
+
+    if (typeof name !== 'undefined') {
+      filter.name = new RegExp('^' + name, 'i');
+    }
+
+    if (typeof price !== 'undefined' && price !== '-') {
+      if (price.indexOf('-') !== -1) {
+        filter.price = {};
+        let range = price.split('-');
+        if (range[0] !== '') {
+          filter.price.$gte = range[0];
+        }
+        if (range[1] !== '') {
+          filter.price.$lte = range[1];
+        }
+      } else {
+        filter.price = price;
+      }
+    }
+
+    if (typeof tag !== 'undefined') {
+      filter.tags = tag;
+    }
+
+    if (typeof onsale !== 'undefined') {
+      filter.onsale = onsale;
+    }
+
+    const adverts = await Advert.list(filter, limit, skip, sort);
+
     res.render('index', {
-      arrayAdvertsDB: arrayAdvertsDB,
+      adverts: adverts,
+      /* arrayAdvertsDB: arrayAdvertsDB */
       title: 'Nodepop',
       description: 'List of all ads stored in the API database.',
     });
   } catch (err) {
-    next(err);
+    {
+      return res.next(err);
+    }
   }
 });
 
